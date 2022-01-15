d45b943d0cf168b1ee6c79f5fd37827da408e2f2
Pavel Trushnikov <oracal@mail.ru>
2022-01-15 18:53:25 +0300

    Реализация закрытия/открытия модального окна Вызов замерщика

--- src/module/modal.js
+++ src/module/modal.js
@@ -2,6 +2,8 @@ const modal = () => {
     const callModal = document.getElementById('call');
     const headerModal = document.querySelector('.header-modal');
     const overlayModal = document.querySelector('.overlay');
+    const servicesBtn = document.querySelectorAll('.service-button');
+    const servicesModal = document.querySelector('.services-modal');
 
     
     callModal.addEventListener('click', (e) => {
@@ -15,6 +17,22 @@ const modal = () => {
         let target = e.target;
         if(target.classList.contains('header-modal__close')) {
             headerModal.style.display = 'none';
+            overlayModal.style.display = 'none';
+        }
+    });
+
+    servicesBtn.forEach(elem => {
+        elem.addEventListener('click', (e) => {
+            e.preventDefault();
+            servicesModal.style.display = 'flex';
+            overlayModal.style.display = 'flex';
+        });
+    });
+    servicesModal.addEventListener('click', (e) => {
+        e.preventDefault();
+        let target = e.target;
+        if(target.classList.contains('services-modal__close')) {
+            servicesModal.style.display = 'none';
             overlayModal.style.display = 'none';
         }
     });
