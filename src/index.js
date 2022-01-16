4c836e8941b7f27b23a2919ab817234e186f8dd8
Pavel Trushnikov <oracal@mail.ru>
2022-01-16 20:40:34 +0300

    Создание модуля slider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import scroll from './module/scroll';
 import modal from './module/modal';
 import timer from './module/timer';
+import slider from './module/slider';
 
 document.addEventListener('DOMContentLoaded', () => {
     scroll();
     modal();
     timer('21 January 2022');
+    slider();
 });
---
+++ src/module/slider.js
@@ -0,0 +1,46 @@
+import Swiper, { Autoplay, Navigation } from 'swiper';
+Swiper.use([Autoplay, Navigation]);
+
+const slider = () => {
+    const sliderOne = new Swiper('.swiperone', {
+        slidesPerView: 1,
+        spaceBetween: 3,
+        centeredSlides: true,
+        slidesPerColumn: 1,
+        loop: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        breakpoints: {
+          576: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        },
+        navigation: {
+          nextEl: ".benefits__arrow--right",
+          prevEl: ".benefits__arrow--left",
+        },
+    });
+    const sliderTwo = new Swiper('.swipertwo', {
+        slidesPerView: 1,
+        loop: true,
+        autoplay: {
+          delay: 2600,
+          disableOnInteraction: false,
+        },
+        breakpoints: {
+          576: {
+            slidesPerView: 2,
+          },
+        },
+        navigation: {
+          nextEl: ".services__arrow--right",
+          prevEl: ".services__arrow--left",
+        },
+    });
+    console.log('Это слайдер');
+};
+
+export default slider;
