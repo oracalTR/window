d274b623b2483523206832783a74ae39bb440875
Pavel Trushnikov <oracal@mail.ru>
2022-01-16 23:19:52 +0300

    Создание модуля slider Правка слайдера на всех страницах

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import scroll from './module/scroll';
 import modal from './module/modal';
 import timer from './module/timer';
 import slider from './module/slider';
+import calc from './module/calc';
 
 document.addEventListener('DOMContentLoaded', () => {
     scroll();
     modal();
     timer('21 January 2022');
     slider();
+    calc();
 });
