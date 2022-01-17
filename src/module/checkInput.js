9837abedc3c42538cf31440e410d1f55c84e60fa
Pavel Trushnikov <oracal@mail.ru>
2022-01-17 21:11:06 +0300

    Создание модуля calc Реализация калькулятора создание переменных

---
+++ src/module/checkInput.js
@@ -0,0 +1,9 @@
+import calc from './calc';
+
+const checkInput = () => {
+    const form = document.querySelectorAll('form');
+
+    const calcType = document.getElementById('calc-type');
+    const calcTypeMaterial = document.getElementById('calc-type-material');
+    const calcInput = document.getElementById('calc-input');
+};
