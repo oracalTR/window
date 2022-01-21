50a0335e6ab374b3a484c478b95fb8f9c64b6c31
Pavel Trushnikov <oracal@mail.ru>
2022-01-21 22:34:23 +0300

    Доработка модуля menu, открытие/закрытие и плавный переход к разделу страницы

--- src/module/menu.js
+++ src/module/menu.js
@@ -1,11 +1,39 @@
+import { animate } from "./helpers";
+import { elementScrollIntoView } from "seamless-scroll-polyfill";
+
 const menu = () => {
     const boxNav = document.getElementById('navigation');
+    const navMenu = document.getElementById('navbar-collapse-fixed');
     
     boxNav.addEventListener('click', (e) => {
         let target = e.target;
         if(target.closest('.navbar-toggle')) {
+            navMenu.classList.remove('collapse');
+            navMenu.classList.remove('navbar-collapse');
+            animate({
+                duration: 500,
+                timing(timeFraction) {
+                    return timeFraction;
+                },
+                draw(progress) {
+                    navMenu.style.opacity = progress;
+                }
+            });
         }
     });
+    navMenu.addEventListener('click', (e) => {
+        let target = e.target;
+        const menuItem = document.getElementById(target.attributes.href.value.substring(1));
+        if(target.closest('li')) {
+            navMenu.classList.add('collapse');
+            navMenu.classList.add('navbar-collapse');
+            elementScrollIntoView(menuItem, {
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+
 };
 
 export default menu;
